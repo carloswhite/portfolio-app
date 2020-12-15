2415946857b52f85d40179840177d89b73341e3a
Carlos Blanco <carlosbusinesinquiries@gmail.com>
2020-12-15 14:58:53 -0500

    Add social icons

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { SocialIcon } from "react-social-icons";
 
 
 export default function NavBar() {
@@ -23,8 +24,11 @@ export default function NavBar() {
                         About Me
                     </NavLink>
                 </nav>
-                <div>
-                    
+                <div className="inline-flex py-3 px-3 my-6">
+                    <SocialIcon url="https://www.linkedin.com/in/carlos-a-blanco01" className="mr-4" target="_blank" fgColor="#fff" style={{height: 35, width: 35}} />
+                    <SocialIcon url="https://github.com/carloswhite" className="mr-4" target="_blank" fgColor="#fff" style={{height: 35, width: 35}} />
+                    <SocialIcon url="https://www.behance.net/carlosblanco5" className="mr-4" target="_blank" fgColor="#fff" style={{height: 35, width: 35}} />
+                    <SocialIcon url="https://dribbble.com/therealcarlosblanco" className="mr-4" target="_blank" fgColor="#fff" style={{height: 35, width: 35}} />
                 </div>
             </div>
         </header>
